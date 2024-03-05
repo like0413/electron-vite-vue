8a8963f565986c7d3434b6f8ba6277f1ffd802f1
like <1575523729@qq.com>
2024-03-05 10:04:17 +0800

    123

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -59,13 +59,15 @@ async function createWindow() {
     },
   })
 
-  if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
-    win.loadURL(url)
-    // Open devTool if the app is not packaged
-    win.webContents.openDevTools()
-  } else {
-    win.loadFile(indexHtml)
-  }
+  // if (process.env.VITE_DEV_SERVER_URL) { // electron-vite-vue#298
+  //   win.loadURL(url)
+  //   // Open devTool if the app is not packaged
+  //   win.webContents.openDevTools()
+  // } else {
+  //   win.loadFile(indexHtml)
+  // }
+
+  win.loadURL('https://www.tingkelai.com/tingkelai')
 
   // Test actively push message to the Electron-Renderer
   win.webContents.on('did-finish-load', () => {
