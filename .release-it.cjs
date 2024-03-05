bbd1429c7e9f20acf73e3e5df74764aa68e0db3c
like <1575523729@qq.com>
2024-03-05 16:53:05 +0800

    999999

--- .release-it.cjs
+++ .release-it.cjs
@@ -3,8 +3,7 @@ module.exports = {
   git: {
     // changelog: 'git log --pretty=format:"* %s (%h)" ${from}...${to}',
     requireCommits: true, // 如果设为true，则需要有新的提交才可以发布
-    // requireCommitsFail: true, // 如果设为true，如果没有新的提交则发布会失败
-    commit: true, // 如果设置为true，则创建一个新的 git commit
+    // commit: true, // 如果设置为true，则创建一个新的 git commit
     commitMessage: 'chore(release): ${version}',
     tag: true,
     // getLatestTagFromAllRefs: false,
