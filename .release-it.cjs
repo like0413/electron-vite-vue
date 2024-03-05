48d8701dc272102ff1d3950f5e7019a4e5c51d8a
like <1575523729@qq.com>
2024-03-05 16:56:53 +0800

    123

--- .release-it.cjs
+++ .release-it.cjs
@@ -4,9 +4,9 @@ module.exports = {
     changelog: 'git log --pretty=format:"* %s (%h)" ${from}...${to}',
     requireCommits: true,
     commit: true, // 自动为package.json的版本号变化创建一个提交
-    commitMessage: 'chore(release): ${version}',
+    commitMessage: 'chore(release): v${version}',
     tag: true,
-    tagAnnotation: 'Release ${version}',
+    tagAnnotation: 'Release v${version}',
   },
   npm: {
     publish: false,
