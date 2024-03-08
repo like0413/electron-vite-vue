6efe3322761abaa5f0dbab0c1f482f4ac622a0e4
like <1575523729@qq.com>
2024-03-08 18:33:07 +0800

    123

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import { Layout, Menu } from 'ant-design-vue'
 import App from './App.vue'
 import router from './router'
 
 import './style.css'
 
 const app = createApp(App)
+
+app.use(Layout)
+app.use(Menu)
 
 app.use(router)
 
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>{{ appInfo }}</div>
+  <a-layout has-sider class="setting">
+    <div class="title">设置</div>
+    <a-layout-sider
+      :style="{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+        top: '45px',
+        bottom: 0,
+        backgroundColor: '#fff',
+      }"
+    >
+      <a-menu mode="inline" class="menu">
+        <a-menu-item key="1">
+          <span class="nav-text">nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <span class="nav-text">nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <span class="nav-text">nav 3</span>
+        </a-menu-item>
+        <a-menu-item key="4">
+          <span class="nav-text">nav 4</span>
+        </a-menu-item>
+        <a-menu-item key="5">
+          <span class="nav-text">nav 5</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+  </a-layout>
 </template>
 
 <script lang="ts" setup>
@@ -35,4 +66,18 @@ function onBootLaunchChange() {
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.setting {
+  .title {
+    height: 45px;
+    line-height: 45px;
+    padding-left: 24px;
+    font-size: 16px;
+  }
+  .menu {
+    width: 100%;
+    height: calc(100vh - 45px);
+    padding: 0 10px;
+  }
+}
+</style>
