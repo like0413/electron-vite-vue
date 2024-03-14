d1fa9fa7dde3e14752ce935c3c34124d5a697c8b
like <1575523729@qq.com>
2024-03-14 15:20:17 +0800

    123

--- electron/main/modules/setContextmenu.ts
+++ electron/main/modules/setContextmenu.ts
@@ -1,4 +1,5 @@
 import { BrowserWindow, Menu } from 'electron'
+import openSetting from './openSetting'
 
 function setMenuTemplate() {
   const menu = Menu.buildFromTemplate([
@@ -16,6 +17,12 @@ function setMenuTemplate() {
       label: '刷新',
       accelerator: 'Shift+CommandOrControl+R',
       role: 'reload',
+    },
+    {
+      label: '设置',
+      click: () => {
+        openSetting()
+      },
     },
   ])
   return menu
