8aa110ee1b666cce9a7042d8030891b48a94ab9a
like <1575523729@qq.com>
2024-03-14 15:04:03 +0800

    123

--- electron/main/modules/createTray.ts
+++ electron/main/modules/createTray.ts
@@ -1,4 +1,4 @@
-import { app, Tray, Menu, BrowserWindow, nativeImage, nativeTheme } from 'electron'
+import { app, Tray, Menu, BrowserWindow, nativeImage, nativeTheme, systemPreferences } from 'electron'
 import openSetting from './openSetting'
 import { getPath } from '../helpers'
 
@@ -6,17 +6,20 @@ let tray: Tray | null = null
 
 function getIcon() {
   let iconPath = ''
+  let size = 16
   if (process.platform === 'darwin') {
-    // if (nativeTheme.shouldUseDarkColors) {
-    // iconPath = getPath('../../build/icon_mac_light.png')
-    // } else {
-    iconPath = getPath('../../build/iconTemplate.png')
-    // }
+    if (nativeTheme.shouldUseDarkColors) {
+      iconPath = getPath('../../build/icon_mac_light.png')
+    } else {
+      iconPath = getPath('../../build/icon_mac_dark.png')
+    }
   } else {
+    size = 64
     iconPath = getPath('../../build/icon.ico')
+    console.log('iconPath: ', iconPath)
   }
 
-  return nativeImage.createFromPath(iconPath).resize({ width: 32, height: 32 })
+  return nativeImage.createFromPath(iconPath).resize({ width: size, height: size })
 }
 
 function createTray(win: BrowserWindow) {
@@ -47,6 +50,13 @@ function createTray(win: BrowserWindow) {
   tray.on('click', () => {
     win.isVisible() ? win.hide() : win.show()
   })
+
+  // mac系统需要监听操作系统的主题变化
+  if (process.platform === 'darwin') {
+    systemPreferences.subscribeNotification('AppleInterfaceThemeChangedNotification', () => {
+      tray && tray.setImage(getIcon())
+    })
+  }
 }
 
 export default createTray
