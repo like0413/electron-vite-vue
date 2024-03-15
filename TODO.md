f87071da4fb4500ad6a028a15273920849c72d49
like <1575523729@qq.com>
2024-03-15 14:55:14 +0800

    123

--- TODO.md
+++ TODO.md
@@ -1,3 +1,8 @@
 1、macos 上不显示 tray
 2、自动更新：代码签名、公证
 3、macos 打开慢、全屏后关闭后黑屏
+
+文章：https://blog.csdn.net/hellodaixy/article/details/132480336
+
+> 签名：https://www.electron.build/code-signing
+> 公证：https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
--- scripts/notarize.js
+++ scripts/notarize.js
@@ -1,3 +1,8 @@
+/**
+ * 这里是 mac 公证
+ * link:https://kilianvalkhof.com/2019/electron/notarizing-your-electron-application/
+ */
+
 import { notarize } from '@electron/notarize'
 import path from 'path'
 
@@ -8,24 +13,22 @@ const { APPLE_ID, APPLE_PASSWORD, APPLE_TEAM_ID } = process.env
 const main = async (context) => {
   const { electronPlatformName, appOutDir } = context
 
-  // 如果不是mac平台，或者不是prod环境，或者没有苹果账号信息，就跳过签名
+  // 如果不是mac平台，或者不是prod环境，或者没有苹果账号信息，就跳过
   if (electronPlatformName === 'win32' || ENV !== 'prod' || !APPLE_ID || !APPLE_PASSWORD || !APPLE_TEAM_ID) {
-    console.log('跳过签名')
+    console.log('跳过公证')
     return
   }
 
-  console.log('开始mac签名')
-
+  console.log('开始公证')
   const appName = context.packager.appInfo.productFilename
-
   await notarize({
     appBundleId: 'prod.tingkelai.app',
-    appPath: `${appOutDir}/${appName}.app`, // windows下是exe文件，mac下是app文件
+    appPath: `${appOutDir}/${appName}.app`,
     appleId: APPLE_ID,
     appleIdPassword: APPLE_PASSWORD,
     teamId: APPLE_TEAM_ID,
   })
-  console.log('签名mac结束')
+  console.log('公证结束')
 }
 
 export default main
