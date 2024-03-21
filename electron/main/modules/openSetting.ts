10585d2222a61e487f93a52ccdf6d339e7d7d69a
like <1575523729@qq.com>
2024-03-21 17:59:07 +0800

    123

--- electron/main/modules/openSetting.ts
+++ electron/main/modules/openSetting.ts
@@ -9,18 +9,18 @@ function openSetting() {
   const preload = join(__dirname, '../preload/index.mjs')
   const icon = join(process.env.VITE_PUBLIC, './icon.png')
 
-  log.info('0:::', __filename)
-  log.info('1:::', __dirname)
-  log.info('2:::', process.env.ROOT)
-  log.info('3:::', process.env.DIST)
-  log.info('4:::', process.env.VITE_PUBLIC)
-  log.info('5:::', join(__dirname, '../preload/index.mjs'))
-  log.info('6:::', join(process.env.ROOT, './preload/index.mjs'))
-  log.info('7:::', join(process.env.VITE_PUBLIC, './icon.png'))
-  log.info('8:::', join(process.env.DIST, './icon.png'))
-  log.info('9:::', app.getAppPath() + '/dist/index.html#/setting')
-  log.info('11:::', join(process.env.DIST, './index.html#/setting'))
-  log.info('12:::', join(__dirname, '../dist/index.html#/setting'))
+  // log.info('0:::', __filename)
+  // log.info('1:::', __dirname)
+  // log.info('2:::', process.env.ROOT)
+  // log.info('3:::', process.env.DIST)
+  // log.info('4:::', process.env.VITE_PUBLIC)
+  // log.info('5:::', join(__dirname, '../preload/index.mjs'))
+  // log.info('6:::', join(process.env.ROOT, './preload/index.mjs'))
+  // log.info('7:::', join(process.env.VITE_PUBLIC, './icon.png'))
+  // log.info('8:::', join(process.env.DIST, './icon.png'))
+  // log.info('9:::', app.getAppPath() + '/dist/index.html#/setting')
+  // log.info('11:::', join(process.env.DIST, './index.html#/setting'))
+  // log.info('12:::', join(__dirname, '../dist/index.html#/setting'))
 
   if (win) {
     win.show()
@@ -42,8 +42,10 @@ function openSetting() {
   if (process.env.VITE_DEV_SERVER_URL) {
     win.loadURL(process.env.VITE_DEV_SERVER_URL + '#/setting')
   } else {
-    win.loadURL(app.getAppPath() + '/dist/index.html#/setting')
+    win.loadURL('file://' + app.getAppPath() + '/dist/index.html#/setting')
   }
+
+  win.webContents.openDevTools()
 
   win.on('closed', () => {
     win.destroy()
